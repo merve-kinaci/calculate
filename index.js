fb2f5a62b0c16e04147cf5c1d85a40f575bf433e
Merve Kınacı <mer.kinaci@gmail.com>
2019-09-29 00:59:14 +0300

    Update and rename js to index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+var xInput, yInput, zInput, tInput, formulInput, formulaArray;
+
+function handleClick(){
+    getInputsValues();
+    formulaParser();
+    calculater();
+    document.getElementById("sonuc").innerHTML=formulaArray;
+}
+
+function getInputsValues(){
+    xInput = document.getElementById("x").value;
+    yInput = document.getElementById("y").value;
+    zInput = document.getElementById("z").value;
+    tInput = document.getElementById("t").value;
+    formulInput = document.getElementById("formul").value;
+}
+
+function formulaParser(){
+    formulaArray = formulInput.split('');
+    formulaArray[0] = parseInt(xInput);
+    formulaArray[2] = parseInt(yInput);
+    formulaArray[4] = parseInt(zInput);
+    formulaArray[6] = parseInt(tInput);
+}
+
+function calculater(){
+    var c;
+    var index = formulaArray.indexOf('*');
+    var index2 = formulaArray.indexOf('/');
+    var index3 = formulaArray.indexOf('+');
+    var index4 = formulaArray.indexOf('-');
+
+    if(index > 0) {
+        c = formulaArray[index-1] * formulaArray[index+1];
+        formulaArray.splice(index-1, 3, c);
+    }else if(index2 > 0) {
+        c = formulaArray[index2-1] / formulaArray[index2+1];
+        formulaArray.splice(index2-1, 3, c);
+    }else if(index3 > 0) {
+        c = formulaArray[index3-1] + formulaArray[index3+1];
+        formulaArray.splice(index3-1, 3, c);
+    }else if(index4 > 0) {
+        c = formulaArray[index4-1] - formulaArray[index4+1];
+        formulaArray.splice(index4-1, 3, c);
+    }
+
+    if(formulaArray.length > 1){
+        calculater();
+    } else {
+        return;
+    }
+}
